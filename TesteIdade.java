e1db2b5712546e7a38a9fe019b7743eb2d4576e0
pamelassantos <pamelalssilva37@gmail.com>
2024-08-28 12:22:22 +0000

    TesteIdade

---
+++ TesteIdade.java
@@ -0,0 +1,17 @@
+Class TesteIdade {
+    public static void main(String[] args) {
+        int idade;
+        idade = 15;
+
+        int idadeNoAnoQueVem = idade + 1;
+
+        int resultado = idade / 2;
+
+       
+        boolean maiorIdade;
+        maiorIdade(idade >= 18);
+
+         boolean tipoSangue;
+         tipoSangue = 'a' == 'A';
+
+        System.out.println(resultado)
